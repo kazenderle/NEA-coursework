dd0109e5c249bd6b27e42fb4406e19e0cb33f836
kazenderle <114064819+kazenderle@users.noreply.github.com>
2022-12-09 17:27:34 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,45 +1,66 @@
 from flask import Flask, render_template, request
 import sqlite3
+from hashlib import pbkdf2_hmac
+import requests
+import json
 
 app = Flask(__name__)
 
-@app.route('/create')
-def create():
-	con = sqlite3.connect('login.db')
-	cur = con.cursor()
-	cur.execute(	"""	CREATE TABLE Users(
-					Username VARCHAR(20) NOT NULL PRIMARY KEY,
-					Password VARCHAR(20) NOT NULL
-						  )
-			""")
-	con.commit()
-	return 'CREATE'
+
+@app.route("/create")
+def hello_world():
+    con = sqlite3.connect("login.db")
+    cur = con.cursor()
+    try:
+        cur.execute("""
+        CREATE TABLE Users(
+        Username VARCHAR(20) NOT NULL PRIMARY KEY,
+	Password VARCHAR(20) NOT NULL)
+        """)
+    except sqlite3.OperationalError as e:
+        return str(e)
+    return "table created"
+
 
 @app.route('/')
 def home():
-        return render_template('login.html')
+    return render_template('login.html')
+
 
 @app.route('/register')
 def register():
     return render_template('signup.html')
 
-@app.route('/signup', methods=['POST'])
+
+@app.route('/register', methods=['POST'])
 def signup():
+
     con = sqlite3.connect('login.db')
+
+    encrypted_password = pbkdf2_hmac(
+        'sha256', bytearray(request.form['loginPassword'], encoding='utf-8'), bytearray(request.form['loginUser'], encoding='utf-8'), 50)
     cur = con.cursor()
     cur.execute("INSERT INTO Users (Username, Password) VALUES (?,?)",
-                    (request.form['un'],request.form['pw']))
+                (request.form['loginUser'], encrypted_password))
     con.commit()
-    return request.form['un'] + ' added'
+    return render_template('signup_success.html'), {"Refresh": "2; url=/ "}
+
 
 @app.route('/login', methods=['POST'])
 def login():
     con = sqlite3.connect('login.db')
+    encrypted_password = pbkdf2_hmac(
+        'sha256', bytearray(request.form['loginPassword'], encoding='utf-8'), bytearray(request.form['loginUser'], encoding='utf-8'), 50)
     cur = con.cursor()
     cur.execute("SELECT * FROM Users WHERE Username=? AND Password=?",
-                    (request.form['un'],request.form['pw']))
+                (request.form['loginUser'], encrypted_password))
     match = len(cur.fetchall())
     if match == 0:
         return "Wrong username and password"
     else:
-        return "Welcome " + request.form['un']
+        res = requests.get("https://finnhub.io/api/v1/quote?symbol=AAPL&token=ce93rq2ad3i49a9hkpkgce93rq2ad3i49a9hkpl0")
+        dict = json.loads(res.text)
+        print(res)
+        print(dict['c'])
+        return render_template('index.html', apple_price=dict['c'])
+
